316f3fbb3f56b1b9e663c8adcebf42ae76ec367a
TinhoFZ <edupassosa18@gmail.com>
2024-09-25 14:51:51 -0300

    ATV 5 realizada

--- Aula_002_Java/src/br/com/Aula_002_Java/ATV5.java
+++ Aula_002_Java/src/br/com/Aula_002_Java/ATV5.java
@@ -1,11 +1,37 @@
 package br.com.Aula_002_Java;
 
+import java.util.Scanner;
+
 public class ATV5 {
 
 	public static void main(String[] args) {
 
+		int numero1, numero2;
 		
-
+		Scanner teclado = new Scanner(System.in);
+		
+		
+		System.out.println("Escreva o primeiro número");
+		numero1 = teclado.nextInt();
+		
+		System.out.println("Escreva o segundo número");
+		numero2 = teclado.nextInt();
+		
+		
+		boolean igual = numero1 == numero2;
+		boolean diferente = numero1 != numero2;
+		boolean maior = numero1 > numero2;
+		boolean menor = numero1 < numero2;
+		boolean maior_igual = numero1 >= numero2;
+		boolean menor_igual = numero1 <= numero2;
+		
+		System.out.println("O primeiro número é igual ao que o segundo? " + igual);
+		System.out.println("O primeiro número é diferente do segundo? " + diferente);
+		System.out.println("O primeiro número é maior do que o segundo? " + maior);
+		System.out.println("O primeiro número é menor do que o segundo? " + menor);
+		System.out.println("O primeiro número é maior ou igual ao segundo? " + maior_igual);
+		System.out.println("O primeiro número é menor ou igual ao segundo? " + menor_igual);
+		
 	}
 
 }
